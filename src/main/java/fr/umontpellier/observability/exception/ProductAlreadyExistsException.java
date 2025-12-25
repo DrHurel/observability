6952c3ea1da49b18356492a32f3fc7062caee8fe
Hurel <jeremyhurel34@gmail.com>
2025-12-25 19:59:23 +0100

    refactor: remove old src directory (moved to app module)
    - All source files moved to app/src
    - Legacy directory no longer needed

--- src/main/java/fr/umontpellier/observability/exception/ProductAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package fr.umontpellier.observability.exception;
-
-public class ProductAlreadyExistsException extends RuntimeException {
-    public ProductAlreadyExistsException(String id) {
-        super("Product already exists with id: " + id);
-    }
-}
--- src/main/java/fr/umontpellier/observability/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package fr.umontpellier.observability.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String email) {
-        super("User already exists with email: " + email);
-    }
-}
